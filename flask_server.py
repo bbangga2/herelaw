0c0fc6ac9bac887852ea7e80461106dd14bbbeb9
kimtaehyuk1 <98taehyukkim@naver.com>
2023-05-09 00:07:40 +0900

    feat: 지금까지 모은 데이터 db연동 + s3 올릴 코드 작성

--- flask_server.py
+++ flask_server.py
@@ -3,6 +3,18 @@ from db import db_inint
 from db import db_insert
 import pymysql as my
 import json
+import boto3
+
+# s3 연결하기 위한 처리(만약 로컬 처리면 키값 넣어야한다.)
+s3 = boto3.resource('s3')
+s3_client = boto3.client('s3')
+
+# 버킷 리스트 획득
+def test_001():
+    bks = [ bk.name for bk in s3.buckets.all() ]
+    #print(bks)
+    return bks
+
 
 
 app = Flask(__name__)
@@ -14,18 +26,29 @@ def index():
 @app.route('/datashoot', methods=['POST'])
 def datashoot():
     
-    print(request.files)
+    # 업로드 이미지 데이터 받아서 저장하기
     filename=None
     if 'image' in request.files:
         file = request.files['image']
         file.save('uploads/' + file.filename)
         filename = file.filename
         print(filename)
+
+        # #s3에 업로드
+        # def test_003(bks):
+        #     for bk in bks:
+        #         if bk == 'ai-project4-taehyuk': # 훗날 버킷 이름
+        #             s3_client.upload_file(f'uploads/{filename}',bk, f'data/{filename}') # 마지막 f'data/{filename}'는 aws상 올릴곳
+
+        # bks = test_001()
+        # # 파일업로드
+        # test_003(bks)
+
         
-    
+    # json 데이터 받기
     data = json.loads(request.form['location'])  # json 데이터와 파일을 함께 처리하려면 request.form으로 받아서 json화 되어있는 location 로드하기
     print(data) # [{'name': 'location', 'value': '고속도로'}, {'name': 'location', 'value': '횡단보도'}, {'name': 'location', 'value': '집앞'}]
-    json_data = json.dumps(data)
+    json_data = json.dumps(data, ensure_ascii=False)
     locations = [item['value'] for item in data if item['name'] == 'location'] # 프론트에서 작성한 form안에 name과 value있었는데 그거통해서
     
 
@@ -35,27 +58,15 @@ def datashoot():
     print(response)  # 결과 : 'message': '교통사고가 발생한 장소는 고속도로, 횡단보도, 집앞입니다.'}
 
     
-    
     # json과 Query를 db에 쏘기
-    # if all((json_data, msg, filename)):
-    #     print("디비왔다")
-    #     db_insert(json_data, msg, filename)
+    if all((json_data, msg, filename)):
+        print("디비왔다")
+        db_insert(json_data, msg, filename)
 
     # 이미지는 s3로 쏘기
 
 
     return '' # jsonify(response) # 리턴을 해줄 필요가 없다.
-
-
-# @app.route('/upload', methods=['POST'])
-# def upload():
-#     if 'image' in request.files:
-#         file = request.files['image']
-#         file.save('uploads/' + file.filename)
-#         print(file.filename)
-#         return ''  # return 'file uploaded successfully'
-#     else:
-#         return '' # return 'no file uploaded'
 
 
 if __name__ == '__main__':
